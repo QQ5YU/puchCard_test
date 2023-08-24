0b5819095437059efde20c992841bce8f2e2da6f
= <=>
2023-08-24 18:06:40 +0800

    新增web.config及server.js檔案

---
+++ server.js
@@ -0,0 +1,27 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3001;
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === "/a") {
+      app.render(req, res, "/a", query);
+    } else if (pathname === "/b") {
+      app.render(req, res, "/b", query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3001, (e) => {
+    if (e) throw e;
+    console.log("Ready on port 3001");
+  });
+});
