85cc662bf55337ebfdf0cc35278c4effe0befcc8
= <=>
2023-09-08 16:14:29 +0800

    remove  nextauth options.ts 內的 pages

--- src/app/api/auth/[...nextAuth]/options.ts
+++ src/app/api/auth/[...nextAuth]/options.ts
@@ -35,5 +35,5 @@ export const options: NextAuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === "development",
-  pages: { signIn: "/login" },
+  // pages: { signIn: "/login" },
 };
