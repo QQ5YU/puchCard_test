62c0bd6975784e1183ef2581751139391d452dd0
= <=>
2023-09-16 11:42:54 +0800

    add Content Security Policy in nextjs config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXTAUTH_SECRET: 'mQ46qpFwfE1BHuqMC+qlm19qBAD9fVPgh28werwe3ASFlAfnKjM=',
+  env: {
+    NEXTAUTH_SECRET: "mQ46qpFwfE1BHuqMC+qlm19qBAD9fVPgh28werwe3ASFlAfnKjM=",
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*", // automatically handles all locales
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=()",
+          },
+        ],
       },
-}
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
