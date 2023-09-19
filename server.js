35c3b13a11ccaa02d9d1fdbdc2be38606718c9f6
= <=>
2023-09-19 20:33:18 +0800

    修改server.js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 81;
 
-const nextAuth = require("next-auth");
-const nextAuthConfig = require("./src/pages/api/auth/[...nextauth]"); // 导入你的 NextAuth.js 配置文件
-
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -16,10 +13,7 @@ app
     const server = express();
     // 配置反向代理以处理 NextAuth.js 路由
     server.set("trust proxy", 1);
-
-    // 配置 NextAuth.js，使用你的配置文件
-    server.use("/api/auth", nextAuth(nextAuthConfig));
-
+    
     server.all("*", (req, res) => {
       return handle(req, res);
     });
